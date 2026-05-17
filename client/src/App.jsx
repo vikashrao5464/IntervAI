aeabe07dd453974d1a808fbef8852c2f0c3c3347
vikash <vikashrao5464@gmail.com>
2026-05-17 13:04:10 +0530

    Add routing setup, Auth page with Google sign-in UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 
 function App() {
   return (
     <div>
-      fgh
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+      </Routes>
     </div>
   )
 }
