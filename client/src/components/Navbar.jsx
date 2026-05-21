52b5e793568480efb3c16151ccafe72c3fd7ce11
vikash <vikashrao5464@gmail.com>
2026-05-21 22:20:37 +0530

    design navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,40 +4,118 @@ import { useSelector } from 'react-redux';
 import { BsRobot } from "react-icons/bs";
 import { FaUserAstronaut } from "react-icons/fa";
 import { BsCoin } from "react-icons/bs";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HiOutlineLogout } from "react-icons/hi";
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
+import axios from 'axios';
+import { serverUrl } from '../App';
+import AuthModel from './AuthModel';
+
+
 
 function Navbar() {
-    const {userData}=useSelector((state)=>state.user);
-  return (
-    <div className='bg-[#f3f3f3] flex justify-center px-4 pt-6'>
 
-        <motion.div
-        initial={{opacity:0,y:-40}}
-        animate={{opacity:1,y:0}}
-        transition={{duration:0.3}}
-        className='w-full max-w-6xl bg-white rounded-[24px] shadow-sm border border-gray-200 px-8 py-4 flex justify-between items-center relative'>
-        <div className='flex items-center gap-3 cursor-pointer'>
-            <div className='bg-black text-white p-2 rounded-lg '>
+    const [showCreditPopup, setShowCreditPopup] = useState(false);
+    const [showUserPopup, setShowUserPopup] = useState(false);
+    const [showAuth, setShowAuth] = useState(false);    
+    const { userData } = useSelector((state) => state.user);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const handleLogout= async()=>{
+        try{
+           await axios.get(serverUrl+'/api/auth/logout',{
+                withCredentials:true
+            })
+            dispatch(setUserData(null));
+            setShowCreditPopup(false);
+            setShowUserPopup(false);
+            navigate('/');
+        }catch(error){
+           console.log("Logout error:",error);
+        }
+    }
+    return (
+        <div className='bg-[#f3f3f3] flex justify-center px-4 pt-6'>
 
-            <BsRobot size={18}/>
-            </div>
-            <h1 className='font-semibold hidden md:block text-lg'>IntervAI</h1>
+            <motion.div
+                initial={{ opacity: 0, y: -40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+                className='w-full max-w-6xl bg-white rounded-[24px] shadow-sm border border-gray-200 px-8 py-4 flex justify-between items-center relative'>
+                <div className='flex items-center gap-3 cursor-pointer'>
+                    <div className='bg-black text-white p-2 rounded-lg '>
+
+                        <BsRobot size={18} />
+                    </div>
+                    <h1 className='font-semibold hidden md:block text-lg'>IntervAI</h1>
+                </div>
+
+                <div className='flex items-center gap-6 relative'>
+                    <div className='relative'>
+                        <button
+                            onClick={() => {
+                                
+                                if(!userData){
+                                    setShowAuth(true);
+                                    return;
+                                }
+                                setShowCreditPopup
+                                (!showCreditPopup);
+                                setShowUserPopup(false)
+                            }}
+                            className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
+                            <BsCoin size={20} />
+                            {userData ? userData.credits || 0 : 0}
+                        </button>
+
+                        {showCreditPopup && (
+                            <div className='absolute right-[-50px] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded-xl p-5 z-50'>
+
+                                <p className='text-sm text-gray-600 mb-4 '>Need More Credits to Continue Interviews?</p>
+                                <button onClick={() => navigate('/pricing')} className='w-full bg-black text-white py-2 rounded-lg text-sm'>Buy more credits</button>
+                            </div>
+                        )}
+                    </div>
+                    <div className='relative'>
+                        <button
+                            onClick={() => {
+                                  if(!userData){
+                                    setShowAuth(true);
+                                    return;
+                                }
+                                setShowUserPopup(!showUserPopup);
+                                setShowCreditPopup(false)
+                            }}
+                            className='w-9 h-9 bg-black text-white rounded-full flex items-center 
+                            justify-center font-semibold'>
+                            {userData ? userData?.name.slice(0, 1).toUpperCase() :
+                                <FaUserAstronaut size={16} />}
+                        </button>
+
+                        {showUserPopup && (
+                            <div className='absolute right-0 mt-3 w-48 bg-white shadow-xl border border-gray-200 rounded-xl p-4 z-50'>
+                                <p className='text-md text-blue-500 font-medium mb-1 '>{userData?.name}</p>
+                                <button onClick={()=>navigate("/history")} className='w-full text-left text-sm py-2 hover:text-black text-gray-600 '>InterView History</button>
+                                <button
+                                onClick={handleLogout}
+                                className='w-full text-left text-sm py-2 flex items-center gap-2 text-red-500 '>
+                                    <HiOutlineLogout size={18} />
+                                    Logout</button>
+                            </div>
+                        )}
+
+                    </div>
+                </div>
+
+
+            </motion.div>
+
+            {showAuth && <AuthModel onClose={() => setShowAuth(false)} />}
+
         </div>
-
-        <div className='flex items-center gap-6 relative'>
-            <div className='relative'>
-                <button className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
-                    <BsCoin size={20} />
-                    {userData ? userData.credits || 0:0}
-                </button>
-            </div>
-            <div></div>
-        </div>
-
-
-        </motion.div>
-      
-    </div>
-  )
+    )
 }
 
 export default Navbar
