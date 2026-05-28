f5aedb5d95e9c9499e9b15a01eccb10891720451
vikash <vikashrao5464@gmail.com>
2026-05-28 16:51:27 +0530

    addind interview report

--- client/src/components/Step3Report.jsx
+++ client/src/components/Step3Report.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import { motion } from 'motion/react'
+import {CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { buildStyles } from 'react-circular-progressbar';
+import {Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
+function Step3Report({ report }) {
 
-function Step3Report({report}) {
-
-  if(!report){
-    return(
+  if (!report) {
+    return (
       <div className='min-h-screen flex items-center justify-center'>
         <p className='text-gray-500 text-lg'>Loading report...</p>
       </div>
@@ -11,41 +17,146 @@ function Step3Report({report}) {
   }
 
   const {
-    finalScore=0,
-    confidence=0,
-    communication=0,
-    correctness=0,
-    questionWiseScore=[]
-  }=report;
+    finalScore = 0,
+    confidence = 0,
+    communication = 0,
+    correctness = 0,
+    questionWiseScore = []
+  } = report;
 
-  const questionScoreData=questionWiseScore.map((item,index)=>({
-    question:`Q${index+1}`,
-    score:item.score || 0
+  const questionScoreData = questionWiseScore.map((item, index) => ({
+    question: item.question || `Q${index + 1}`,
+    score: Number(item.score) || 0
   }));
 
-  const skills=[
-    {label:'Confidence',value:confidence},
-    {label:'Communication',value:communication},
-    {label:'Correctness',value:correctness}
+  const skills = [
+    { label: 'Confidence', value: confidence },
+    { label: 'Communication', value: communication },
+    { label: 'Correctness', value: correctness }
   ];
 
-  let performanceTask="";
-  let shortTagline="";
+  let performanceTask = "";
+  let shortTagline = "";
 
-  if(finalScore>=8){
-    performanceTask="Ready for job oppurnities.";
-    shortTagline="Excellent Clarity and Structured Responses.";
-  }else if(finalScore>=5){
-    performanceTask="Needs minor improvement before interviews.";
-    shortTagline="Good understanding but work on clarity.";
-  }else{
-    performanceTask="significant improvement needed.";
-    shortTagline="Work on clarity and confidence.";
+  if (finalScore >= 8) {
+    performanceTask = "Ready for job oppurnities.";
+    shortTagline = "Excellent Clarity and Structured Responses.";
+  } else if (finalScore >= 5) {
+    performanceTask = "Needs minor improvement before interviews.";
+    shortTagline = "Good understanding but work on clarity.";
+  } else {
+    performanceTask = "significant improvement needed.";
+    shortTagline = "Work on clarity and confidence.";
   }
-   
-  return ( 
+
+  const score= finalScore;
+  const percentage = (score / 10) * 100;
+
+  const navigate = useNavigate();
+
+  return (
     <div className='min-h-screen bg-linear-to-br from-gray-50 to-green-50 px-4 sm:px-6 lg:px-10 py-8'>
-       fdghj
+      <div className='mb-8 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4'>
+        {/* heading */}
+        <div className='md:mb-10 w-full flex items-start gap-4 flex-wrap'>
+          <button
+            onClick={() => navigate('/history')}
+            className='mt-1 p-3 rounded-full bg-white-shadow hover:shadow-md transition'>
+            <FaArrowLeft className='text-gray-600' />
+          </button>
+
+          <div>
+            <h1 className='text-3xl font-bold text-gray-800 flex-nowrap '>Interview Analysis Dashboard</h1>
+            <p className='text-gray-500 mt-2'>AI-Powered performance insights</p>
+          </div>
+        </div>
+
+        <button className='bg-emerald-600 hover:bg-emerald-700 text-white py-3 rounded-xl shadow-md
+                           transition-all duration-300 font-semibold text-sm sm:text-base text-nowrap p-6'>Download PDF</button>
+      </div>
+
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8'>
+        <div className='space-y-6'>
+          <motion.div
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          className="bg-white rounded-2xl sm:rounded-3xl shadow-lg p-6 sm:p-8 text-center"
+          >
+
+            <h3 className='text-gray-500 mb-4 sm:mb-6 text-sm sm:text-base '>Overall Performance</h3>
+          <div className='relative w-20 h-20 sm:w-25 sm:h-25 mx-auto '>
+                <CircularProgressbar
+                 value={percentage}
+                 text={`${score}/10`}
+                 styles={buildStyles({
+                  textSize:"18px",
+                  pathColor:"10b981",
+                  textColor:"#ef4444",
+                  trailColor:"#e5e7eb"
+                 })}
+                 />
+              </div>
+              <p className='text-gray-400 mt-3 text-xs sm:text-sm '>Out of 10</p>
+              <div className='mt-4'>
+                <p className='font-semibold text-gray-800 text-sm sm:text-base'>{performanceTask}</p>
+                <p className='text-gray-500 text-xs sm:text-sm mt-1 '>{shortTagline}</p>
+              </div>
+          </motion.div>
+
+          <motion.div
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          className='bg-white rounded-2xl sm:rounded-3xl shadow-lg p-6 sm:p-8'
+          >
+            <h3 className='text-base sm:text-lg font-semibold text-gray-700 mb-6'>Skill Evaluation</h3>
+
+            <div className='space-y-5'>
+              {
+                skills.map((s,i)=>(
+                  <div key={i}>
+
+                    <div className='flex justify-between mb-2 text-sm sm:text-base'>
+                      <span>{s.label}</span>
+                      <span className='font-semibold text-green-600 '>{s.value}</span>
+                    </div>
+                 
+                  <div className='bg-gray-200 h-2 sm:h-3 rounded-full '>
+                    <div className='bg-green-500 h-full rounded-full ' style={{width: `${s.value*10}%`}}></div>
+                  </div>
+
+                  </div>
+                ))
+              }
+            </div>
+
+          </motion.div>
+        </div>
+        <div className='lg:col-span-2 space-y-6'>
+          <motion.div
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          className='bg-white rounded-2xl sm:rounded-3xl shadow-lg p-5 sm:p-8'
+          >
+            <h3 className='sm:text-base text-lg font-semibold text-gray-700 mb-4 sm:mb-6 '>Performance Trend</h3>
+           <div className='h-64 sm:h-72'>
+            <ResponsiveContainer width="100%" height="100%">
+            <AreaChart data={questionScoreData}>
+            <CartesianGrid strokeDasharray="3 3" />
+                 <XAxis dataKey="question" />
+            <YAxis domain={[0, 10]} />
+            <Tooltip />
+            <Area type="monotone"
+                  dataKey="score"
+                   stroke="#22c55e"
+                   fill="#bbf7d0" 
+                   strokeWidth={3} />
+            </AreaChart>
+
+            </ResponsiveContainer>
+           </div>
+          </motion.div>
+        </div>
+      </div>
     </div>
   )
 }
