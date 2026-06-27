633280215891eb91620ac623000ea1767980c063
vikash <vikashrao5464@gmail.com>
2026-06-27 09:51:17 +0530

    add public folder

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -18,8 +18,8 @@ export const googleAuth=async(req,res)=>{
     // Set cookie
     res.cookie("token", token, {
          httpOnly: true,
-         secure: false,
-        sameSite: "strict",
+         secure: true,
+        sameSite: "none",
         maxAge: 7*24 * 60 * 60 * 1000, });
     res.status(200).json(user);
 } catch (error) {
