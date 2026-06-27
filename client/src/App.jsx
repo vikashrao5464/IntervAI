a9ca84c2be8e2f5fe23eecc22ec79a0fecec7324
vikash rao <168458691+vikashrao5464@users.noreply.github.com>
2026-06-27 10:01:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import { useEffect } from 'react';
 import axios from 'axios';
-export const serverUrl="http://localhost:8000";
+export const serverUrl="https://intervai-1sr7.onrender.com";
 import { useDispatch } from 'react-redux';
 import { setUserData } from './redux/userSlice';
 import InterviewPage from './pages/InterviewPage';
